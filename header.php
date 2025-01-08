4118a677ac8beb4be043abe1643a2dc5d7dbe64c
Veronica <no.demchenko@yandex.ru>
2025-01-08 18:03:52 +0300

    feat(header): добавила Header

--- header.php
+++ header.php
@@ -21,39 +21,36 @@
 </head>
 
 <body <?php body_class(); ?>>
+
 <?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'interno_wp' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$interno_wp_description = get_bloginfo( 'description', 'display' );
-			if ( $interno_wp_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $interno_wp_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'interno_wp' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+<header>
+        <div class="container">
+            <div class="header">
+                <a href="#" class="logo">
+                    <img src="/img/logo.svg" alt="Interno Logo" />
+                </a>
+                <nav>
+                    <ul class="ul header__nav">
+                        <li class="header__item">
+                            <a class="header__link" href="#">Home</a>
+                        </li>
+                        <li class="header__item">
+                            <a class="header__link" href="#">Our Projects</a>
+                        </li>
+                        <li class="header__item">
+                            <a class="header__link" href="#">Blog</a>
+                        </li>
+                        <li class="header__item">
+                            <a class="header__link" href="#">Contact</a>
+                        </li>
+                        <li class="header__item">
+                            <button class="header__search" onclick="openModal();">
+                                <img src='/img/icon/search-icon.svg' alt="Search button" />
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+                <button class="header__menu" onclick="openModalMenu();"></button>
+            </div>
+        </div>
+    </header>
