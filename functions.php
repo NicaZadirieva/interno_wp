f2e34471e38f472dc3e35117f4c84a53bb7422a4
Veronica <no.demchenko@yandex.ru>
2025-01-09 14:59:23 +0300

    chore: enable jQuery

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 function interno_wp_enqueue_scripts() {
 	wp_enqueue_style('interno-wp-style', get_template_directory_uri().'/css/index.css', array(), '2025-01-08', 'all');
 	wp_enqueue_script('swiper', get_template_directory_uri().'/swiper/swiper-bundle.min.js', array('jquery'), '1.0.0', true);
-    wp_enqueue_script('interno-wp-script', get_template_directory_uri().'/script/index.js', array('swiper'), '1.0.0', true);
+    wp_enqueue_script('interno-wp-script', get_template_directory_uri().'/script/index.js', array('swiper', 'jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'interno_wp_enqueue_scripts');
 
