7e60c55b7ae9b84585a5ac360661cda694e48a0d
Veronica <no.demchenko@yandex.ru>
2025-01-10 10:18:44 +0300

    feat(functions.php): add style only for home page

--- functions.php
+++ functions.php
@@ -8,9 +8,15 @@
  */
 
 function interno_wp_enqueue_scripts() {
-	wp_enqueue_style('interno-wp-style', get_template_directory_uri().'/css/index.css', array(), '2025-01-08', 'all');
+	wp_enqueue_style('interno-wp-style', get_template_directory_uri().'/css/index.css', array(), '2025-01-10', 'all');
 	wp_enqueue_script('swiper', get_template_directory_uri().'/swiper/swiper-bundle.min.js', array('jquery'), '1.0.0', true);
     wp_enqueue_script('interno-wp-script', get_template_directory_uri().'/script/index.js', array('swiper', 'jquery'), '1.0.0', true);
+
+	wp_register_style('interno-wp-style-index', get_template_directory_uri().'/css/main/index.css', array(), '2025-01-10', 'all');
+	
+	if (is_home()) {
+		wp_enqueue_style('interno-wp-style-index');
+	}
 }
 add_action('wp_enqueue_scripts', 'interno_wp_enqueue_scripts');
 
